2ab444b5cd866f019ef8407e02542fea6ddcddab
Mark Toujiline <mark.toujiline@gmail.com>
2020-01-18 02:42:06 -0800

    Try again

--- scripts/decrypt-keys.sh
+++ scripts/decrypt-keys.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
-openssl aes-256-cbc -K $encrypted_40bed682e53c_key -iv $encrypted_40bed682e53c_iv -in travis-deploy-key.enc -out travis-deploy-key -d
+openssl aes-256-cbc -K $encrypted_40bed682e53c_key -iv $encrypted_40bed682e53c_iv -in travis-deploy-key.enc -out travis-deploy-key -d;
 chmod 600 travis-deploy-key;
 cp travis-deploy-key ~/.ssh/id_rsa;
